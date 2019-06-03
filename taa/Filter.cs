dd3701616f0ff08dee75cae0cc102f812789db51
xztaityozx <taityonohibi@gmail.com>
2019-06-03 16:00:55 +0900

    Filterつくるよ

--- taa/Filter.cs
+++ taa/Filter.cs
@@ -10,5 +10,16 @@ using YamlDotNet.Serialization;
 namespace taa {
 
     public class Filter {
+        private readonly Config config;
+        public Filter(Config config) {
+            this.config = config;
+        }
+
+        public void Build() {
+            var keys = new List<string>();
+            
+            foreach (var condition in config.Conditions) {
+            }
+        }
     }
 }
